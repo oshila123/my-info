0f0a5316a61abbf3de967a402db2a839379f7d81
oshila123 <oshila.work@gmail.com>
2019-12-21 07:24:28 +0530

    Updates

---
+++ precache-manifest.e47f3bc27e3b198344bf936f64eeaabe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68d9a6976f59dc085cf997da73fa10e",
+    "url": "/bio/index.html"
+  },
+  {
+    "revision": "e174ef5975029baa9226",
+    "url": "/bio/static/css/main.b0916541.chunk.css"
+  },
+  {
+    "revision": "88f2f86c50736427b534",
+    "url": "/bio/static/js/2.7b689f54.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/bio/static/js/2.7b689f54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e174ef5975029baa9226",
+    "url": "/bio/static/js/main.85068c44.chunk.js"
+  },
+  {
+    "revision": "ab9ab548af5b465bd9fe",
+    "url": "/bio/static/js/runtime-main.9ec8e8c9.js"
+  }
+]);
